c9ea1c06728789473d145f1ce6b281281e5d6759
benson <bensondawn@163.com>
2019-06-06 13:18:43 +0800

    Springboot学习总结！

--- SpringbootMybatisGenerator/src/main/java/com/benson/mybatis/mapper/UserMapper.java
+++ SpringbootMybatisGenerator/src/main/java/com/benson/mybatis/mapper/UserMapper.java
@@ -4,7 +4,9 @@ import com.benson.mybatis.entity.User;
 import com.benson.mybatis.entity.UserExample;
 import java.util.List;
 import org.apache.ibatis.annotations.Param;
+import org.springframework.stereotype.Component;
 
+@Component
 public interface UserMapper {
     long countByExample(UserExample example);
 
